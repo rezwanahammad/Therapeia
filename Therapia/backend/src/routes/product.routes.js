ec9cdaddf94b0bcaa35a608e76e0d78f9849f491
Rezuan <hasanideal2002@gmail.com>
2025-10-05 10:21:16 +0600

    Add product form updated

--- Therapia/backend/src/routes/product.routes.js
+++ Therapia/backend/src/routes/product.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { listProducts, createProduct } = require('../controllers/product.controller');
+const { listProducts, createProduct, updateProduct, deleteProduct } = require('../controllers/product.controller');
 
 router.get('/', listProducts);
 router.post('/', createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
 
 module.exports = router;
